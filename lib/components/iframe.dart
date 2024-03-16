ddebb7d02f837378f8fe308310fcea5a7225670f
toseefali <toseef.ali@zeeve.io>
2024-03-16 15:10:04 +0530

    added redirect url for emulators

--- lib/components/iframe.dart
+++ lib/components/iframe.dart
@@ -1,7 +1,5 @@
-import 'dart:html';
-import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class IFrame extends StatefulWidget {
   final bool isAndroid;
@@ -13,9 +11,6 @@ class IFrame extends StatefulWidget {
 }
 
 class _IFrameState extends State<IFrame> {
-  Widget? _iframeWidget;
-  Widget? _iframeWidgetiOS;
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -25,60 +20,37 @@ class _IFrameState extends State<IFrame> {
         width: 300,
         child: Stack(
           children: [
-            AnimatedOpacity(
-                opacity: widget.isAndroid ? 1 : 0,
-                duration: const Duration(milliseconds: 300),
-                child: _iframeWidget!),
-            AnimatedOpacity(
-                opacity: widget.isAndroid ? 0 : 1,
-                duration: const Duration(milliseconds: 300),
-                child: _iframeWidgetiOS!),
+            widget.isAndroid
+                ? Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        Uri url = Uri.parse(
+                            'https://appetize.io/embed/ag_mxfsgeig72bumo6lyrpu3per54?device=pixel4&osVersion=12.0&scale=70&autoplay=true');
+                        if (await canLaunchUrl(url)) {
+                          launchUrl(url);
+                        }
+                      },
+                      child: const Text('Tap to redirect to Android emulator'),
+                    ),
+                  )
+                : Container(),
+            widget.isAndroid
+                ? Container()
+                : Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        Uri url = Uri.parse(
+                            'https://appetize.io/embed/ag_ihj33wjg6a7efmoh4mqv5fcmoa?scale=66&autoplay=true');
+                        if (await canLaunchUrl(url)) {
+                          launchUrl(url);
+                        }
+                      },
+                      child: const Text('Tap to redirect to iOS simulator'),
+                    ),
+                  ),
           ],
         ),
       ),
-    );
-  }
-
-  @override
-  void initState() {
-    init();
-    super.initState();
-  }
-
-  init() {
-    final IFrameElement _iframeElement = IFrameElement();
-    _iframeElement.height = '700';
-    _iframeElement.width = '320';
-    // autoplay = true in prod
-    _iframeElement.src =
-        'https://appetize.io/embed/ag_mxfsgeig72bumo6lyrpu3per54?device=pixel4&osVersion=12.0&scale=70';
-    _iframeElement.style.border = 'none';
-    ui.platformViewRegistry.registerViewFactory(
-      'iframeElement',
-      (int viewId) => _iframeElement,
-    );
-
-    _iframeWidget = HtmlElementView(
-      key: UniqueKey(),
-      viewType: 'iframeElement',
-    );
-
-    // for iOS
-    final IFrameElement _iframeElement2 = IFrameElement();
-    _iframeElement2.height = '700';
-    _iframeElement2.width = '320';
-    // autoplay = true in prod
-    _iframeElement2.src =
-        'https://appetize.io/embed/ag_ihj33wjg6a7efmoh4mqv5fcmoa?scale=66&autoplay=true';
-    _iframeElement2.style.border = 'none';
-    ui.platformViewRegistry.registerViewFactory(
-      'iframeElement2',
-      (int viewId) => _iframeElement2,
-    );
-
-    _iframeWidgetiOS = HtmlElementView(
-      key: UniqueKey(),
-      viewType: 'iframeElement2',
     );
   }
 }
